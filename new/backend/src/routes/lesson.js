d6aada8ae6a3c8bd9e12e5e1f116b9821d19e6ce
qwedftfcf <3290442139@qq.com>
2025-12-25 14:57:08 +0800

    实现视频功能

--- new/backend/src/routes/lesson.js
+++ new/backend/src/routes/lesson.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/lessonController');
 
 router.get('/:id/playlist', controller.getPlaylist);
-router.get('/:id/segment/:file', controller.getSegment);
+router.get('/:id/:file', controller.getSegment);
 
 module.exports = router;
